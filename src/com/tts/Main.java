7d4ec03b0709cb664e203c9ff8ae98bbfecfe164
nmvilla15 <niki.villalpando@gmail.com>
2021-01-27 22:28:59 -0600

    first commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,16 @@
+package com.tts;
+import java.util.Arrays;
+import java.lang.Math;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myArray[] = {7, 3, 12, 9, 15};
+        int sum = 0;
+        for (int num : myArray) {
+            sum = sum + num;
+        }
+        System.out.println("The sum of this array is: " + sum);
+    }
+}
